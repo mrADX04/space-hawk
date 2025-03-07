2d07c52553680b75383b574a291e705a91026283
user <mr.adx04mrworldwide@gmail.com>
2025-03-08 02:47:23 +0530

    Projectiles

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -23,7 +23,7 @@ public class Health : MonoBehaviour
             health -= damage;
             if (health <= 0)
             {
-                Destroy(gameObject);
+                Destroy(gameObject); 
             }
         }
     }
